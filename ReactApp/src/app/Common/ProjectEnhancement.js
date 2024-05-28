5ed90388e7bf61bf5ff2197301da135553f1a3ac
Ashish <ash.ishu025@gmail.com>
2024-05-28 22:03:59 +0530

    task work

---
+++ ReactApp/src/app/Common/ProjectEnhancement.js
@@ -0,0 +1,15 @@
+//Task 28th May2024
+
+// Hobby - Additional Task
+//1. Create a component with name Hobby, add a textbox to insert hobbyname into it and save it to the database using SaveAPI
+//2. Create an action to save the hobby, can use useractions file and userrouter file for the same
+
+//Fetch the saved hobbies and show them in Login Page so that user can select any hobby and update the user info
+
+
+//- StudentReact > Component, Route, NavLink,Action, Reducer,
+//StudentName, Age, Address, etc
+
+// -StudentAPI >
+// StudentDataModel, Router/API/Endpoints to save and fetch students
+// -ExpressApp for students and do mounting
