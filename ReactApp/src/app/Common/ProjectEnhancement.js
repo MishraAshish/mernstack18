8912ceb52596513b5954668d396475f06b645a32
Ashish <ash.ishu025@gmail.com>
2024-05-30 21:38:23 +0530

    Product Component

--- ReactApp/src/app/Common/ProjectEnhancement.js
+++ ReactApp/src/app/Common/ProjectEnhancement.js
@@ -1,3 +1,13 @@
+
+// 30-05-2024 - Create Product and Display Them
+
+// Create a product component using functional component and hooks (try using useRef as react hook and implement accordingly)
+// Create a form to allow user to submit Product Details - name, price, desc, rating
+// Create an action method to add the detail to database using a server api
+// Server Side - Create product router and api to save the product using productdatamodel
+// Everything should be done in continuation with shopping cart project
+
+
 //Task 28th May2024
 
 // Hobby - Additional Task
