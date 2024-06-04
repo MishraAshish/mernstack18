eb2a070243aca35a8744a5b558bc87f2fea22a70
Ashish <ash.ishu025@gmail.com>
2024-06-04 22:28:16 +0530

    checkout component

--- ReactApp/src/app/Common/ProjectEnhancement.js
+++ ReactApp/src/app/Common/ProjectEnhancement.js
@@ -1,4 +1,30 @@
 
+// 04-06-2024 - Checkout Component
+
+// Checkout Component
+// Create A functional component and use react hook or using container to read data from store
+// Show - User Details (Name, address) //We will deliver products to below address kind of message as well
+// Show Table of cart put up for purchase (you need to re-use the cartlist and cartitem components)
+// Show the purchase Summary (total qty and total amount)
+// Show a Button to Proceed to Payment
+// Integrate this page on CartComponent Button (Go to checkout) -(Go To Checkout From Cart Component)
+
+// Second Task :
+// Create a state using useState to show hide (Make Payment Message)
+// Upon Clicking on MakePayment button, hide everything and just show the message - "Thankyou for the payment, your items under process!"
+// Change the header from Checkout Page to Payment Page
+
+// 31-05-2024 - Cart Implementation
+
+// Cart Implementation
+// Create New Cart Component using react hooks, functional component
+// Each Item in this component should be added from Product Component 
+// Now each Product should have general fields, like Name, Description, Rating, Price, Category (New Product Document/ Collection)
+// In Product component each item when we click to display details should also have a button "Add To Item" on click should add to New Cart
+// On Cart Component, Button for save to Checkout should save the cart item to database (New Cart Document/ Collection)
+
+
+
 // 30-05-2024 - Create Product and Display Them
 
 // Create a product component using functional component and hooks (try using useRef as react hook and implement accordingly)
