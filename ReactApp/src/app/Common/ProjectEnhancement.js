671c7d5fdf2c10bf8f4449924295095bb11ffc69
Ashish <ash.ishu025@gmail.com>
2024-06-11 21:47:42 +0530

    recenet order page

--- ReactApp/src/app/Common/ProjectEnhancement.js
+++ ReactApp/src/app/Common/ProjectEnhancement.js
@@ -1,3 +1,12 @@
+// 11-06-2024 - Recentorder Page 
+// Upon MakePayment Click
+// Save the cart to RecentOrders collection (should have userid, order, dateTime)
+// Make API to Save and Fetch from RecentOrders
+// Make a component RecentOrders to Show all previous Orders of current user
+// Add a button to Cancel (like) we have remove in CartComponent and then save again, 
+// order can be cancelled within 2 days after that it should be marked delivered
+
+
 // 10-06-2024 - Coupon Page
 
 // Coupon Page 
