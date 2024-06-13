34b36e4a48fcb0d9345605c58bf637337c34d10f
Ashish <ash.ishu025@gmail.com>
2024-06-13 21:47:55 +0530

    product review

--- ReactApp/src/app/Common/ProjectEnhancement.js
+++ ReactApp/src/app/Common/ProjectEnhancement.js
@@ -1,3 +1,11 @@
+// 12-06-2024 - Review Page
+// This should get its reviews from recent orders page
+// User should be allowed to give ratings and his comments to each products
+// Upon successful submission each product should have a link to show its review 
+// When user expands product detail we should also see the button to which will take us to its review
+// on recent order page we can show a popup to submit rating or a new page its up to you //can use -> react bootstrap
+// user should only be able to give rating once cancel button is gone
+
 // 12-06-2024 - Reorder Page
 // Reorder
 // User to reorder from recent orders or from cancelled orders
